92d0ebe4ab21827f17e9ce86720bd3e3ea2c9f00
Scott Angus <scottdangus@gmail.com>
2019-08-07 20:52:09 +0900

    Attempting to fix broken (personal heroku.

--- server/knex.js
+++ server/knex.js
@@ -14,9 +14,11 @@ const l = {
 };
 const db = knex({
   client: "pg",
-  connection: `postgres://${l.user}:${l.password}@${process.env.DB_HOST}:${
-    l.port
-  }/${l.database}`,
+  connection:
+    process.env.DATABASE_URL ||
+    `postgres://${l.user}:${l.password}@${process.env.DB_HOST}:${l.port}/${
+      l.database
+    }`,
   searchPath: "public",
   migration: {
     tablename: "knex_migrations",
