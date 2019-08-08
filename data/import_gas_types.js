90bee7d0bc3a00afe38d4d11eee30bb0fad30a6b
Scott Angus <scottdangus@gmail.com>
2019-08-08 12:26:04 +0900

    Wrote files to fill database tables RESTAURANTS and GAS_TYPES.

---
+++ data/import_gas_types.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const db = require("../server/knex.js");
+const path = require("path");
+let gasTypes = new Set();
+
+(async () => {
+  try {
+    const locations = JSON.parse(
+      fs.readFileSync(__dirname + "/locations.json")
+    );
+    for (const location of locations) {
+      if (location.Site.FuelPrices !== undefined) {
+        for (const fuelPrice of location.Site.FuelPrices) {
+          gasTypes.add(fuelPrice.FuelType);
+        }
+      }
+    }
+    for (const key of gasTypes) {
+      const result = await db("gas_types").insert({
+        name: key,
+      });
+      console.log(result);
+    }
+  } catch (err) {
+    console.error("Error inserting records", err);
+  }
+})();
