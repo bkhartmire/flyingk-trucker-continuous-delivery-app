40506e5183a482318e0eb058d41526af9915ddb3
Brittany Hartmire <bkhartmire@gmail.com>
2019-08-08 14:12:04 +0900

    back to original redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
 const defaultState = {
-  locations: [{ id: null, long: null, lat: null, name: null }],
-  restaurants: [{ id: null, name: null, icon: null }],
-  gasTypes: [{ id: null, name: null }],
-  amenities: [{ id: null, name: null }],
-  locationRestaurants: [{ id: null, locationID: null, restaurantID: null }],
-  locationGasTypes: [{ id: null, locationID: null, gasTypeID: null }],
-  locationAmenities: [{ id: null, locationID: null, restaurantID: null }],
+  locations: [],
 };
+// locations: [{ id: null, long: null, lat: null, name: null }],
+// restaurants: [{ id: null, name: null, icon: null }],
+// gasTypes: [{ id: null, name: null }],
+// amenities: [{ id: null, name: null }],
+// locationRestaurants: [{ id: null, locationID: null, restaurantID: null }],
+// locationGasTypes: [{ id: null, locationID: null, gasTypeID: null }],
+// locationAmenities: [{ id: null, locationID: null, restaurantID: null }],
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
