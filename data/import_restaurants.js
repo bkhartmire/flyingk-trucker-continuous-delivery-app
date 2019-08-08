b605ed7a044527d5fea9e0c313e5c4d84453887e
Scott Angus <scottdangus@gmail.com>
2019-08-08 18:24:04 +0900

    Fixed restaurants id numbers, now using the ids provided by the locations.json file.

--- data/import_restaurants.js
+++ data/import_restaurants.js
@@ -11,15 +11,18 @@ let restaurantObj = {};
     for (const location of locations) {
       if (location.Site.Concepts !== undefined) {
         for (const concept of location.Site.Concepts) {
-          restaurantObj[String(concept.Concept.Name)] =
-            concept.Concept.ConceptIcon;
+          restaurantObj[concept.Concept.Id] = {
+            name: String(concept.Concept.Name),
+            icon: concept.Concept.ConceptIcon,
+          };
         }
       }
     }
     for (const key in restaurantObj) {
       const result = await db("restaurants").insert({
-        name: key,
-        icon: restaurantObj[key],
+        id: key,
+        name: restaurantObj[key].name,
+        icon: restaurantObj[key].icon,
       });
       console.log(result);
     }
