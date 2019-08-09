1aadb819fe7d7dcbb6e70c3ae91982fcd634f3b4
Matthew Cook <mc255v@gmail.com>
2019-08-09 17:41:55 +0900

    updated City select in FilterByLocation

--- src/components/FilterByLocations.jsx
+++ src/components/FilterByLocations.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
 export default class FilterByLocations extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: false,
+    };
+  }
+
+  updateCities(value) {
+    this.setState({ value });
+  }
+
   render() {
     return (
       <div>
         <span>Locations: </span>
-        <select onChange={() => {}}>
+        <select onChange={(event) => this.updateCities(event.target.value)}>
           <option value="">--State--</option>
           {Object.keys(this.props.states).map((state, index) => {
             return (
@@ -16,7 +27,17 @@ export default class FilterByLocations extends Component {
           })}
         </select>
         <select onChange={() => {}}>
-          <option value="">--City--</option>
+          {this.state.value ? (
+            [...this.props.states[this.state.value]].map((city, index) => {
+              return (
+                <option key={index + "a"} value={city}>
+                  {city}
+                </option>
+              );
+            })
+          ) : (
+            <option value="">--City--</option>
+          )}
         </select>
         <select onChange={() => {}}>
           <option value="">--Highway--</option>
