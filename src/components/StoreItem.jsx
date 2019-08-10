4d1d5c79521512cbfaf50dca91697ce465b07ba1
rinasakamaki <h0508g@gmail.com>
2019-08-10 19:17:38 +0900

    test

--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./StoreItem.css";
 import { SlideDown } from "react-slidedown";
 import "react-slidedown/lib/slidedown.css";
+import Subway from "../images/subway.png";
 
 export default class StoreItem extends Component {
   constructor(props) {
@@ -31,18 +32,19 @@ export default class StoreItem extends Component {
     return (
       <div className="storeitem">
         <div className="storebox">
-
+          this.props.location.restaurants[0].name === "Subway" ?{" "}
+          <img src={Subway} alt="logo" />
           <span>{this.props.location.preferredName} - </span>
           <span>{this.props.location.siteName}</span>
           {this.gasItem(this.props.location.gasTypes[0])}
           {this.gasItem(this.props.location.gasTypes[3])}
           <button onClick={() => this.handleClick()}>Show More</button>
+          <p>{this.props.location.restaurants[0].name}</p>
           <SlideDown closed={this.state.closed}>
             {this.props.location.gasTypes.map((type, index) => {
               return this.gasItem(type, index);
             })}
           </SlideDown>
-
         </div>
       </div>
     );
