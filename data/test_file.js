0535d530ebcdf743df448f0b484547627f86c0d1
Matthew Cook <mc255v@gmail.com>
2019-08-10 22:32:41 +0900

    refactored to use JSON in db :P

--- data/test_file.js
+++ data/test_file.js
@@ -1,19 +1,19 @@
 const fs = require("fs");
 const db = require("../server/knex.js");
-const path = require("path");
 
 (async () => {
   try {
     const locations = JSON.parse(
       fs.readFileSync(__dirname + "/locations.json")
     );
-    for (const location of locations) {
-      const site = location.Site;
-      const result = await db("data").insert({
-        site,
-      });
-      console.log(result);
-    }
+    console.log(locations[1]);
+    // for (const location of locations) {
+    //   const site = location;
+    //   const result = await db("data").insert({
+    //     site,
+    //   });
+    //   console.log(result);
+    // }
   } catch (err) {
     console.error("Error inserting records", err);
   }
