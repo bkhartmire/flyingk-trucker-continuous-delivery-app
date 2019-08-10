dc5d739bdbe67137665d618934ee90e223af0520
Matthew Cook <mc255v@gmail.com>
2019-08-10 14:02:58 +0900

    implemented drop down for see more, still working on building storeItem

--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -1,13 +1,46 @@
 import React, { Component } from "react";
 import "./StoreItem.css";
+import { SlideDown } from "react-slidedown";
+import "react-slidedown/lib/slidedown.css";
 
 export default class StoreItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      closed: true,
+    };
+  }
+
+  handleClick() {
+    this.setState({ closed: !this.state.closed });
+  }
+
+  gasItem(type) {
+    return type ? (
+      <ul>
+        <li>{type.name}</li>
+        <li>{type.cash_price}</li>
+        {type.credit_price !== -1 && <li>{type.credit_price}</li>}
+      </ul>
+    ) : (
+      <div />
+    );
+  }
+
   render() {
     return (
       <div className="storeitem">
         <div className="storebox">
           <span>{this.props.location.preferredName} - </span>
-          <span>{this.props.location.site_name}</span>
+          <span>{this.props.location.siteName}</span>
+          {this.gasItem(this.props.location.gasTypes[0])}
+          {this.gasItem(this.props.location.gasTypes[3])}
+          <button onClick={() => this.handleClick()}>Show More</button>
+          <SlideDown closed={this.state.closed}>
+            {this.props.location.gasTypes.map((type) => {
+              return this.gasItem(type);
+            })}
+          </SlideDown>
         </div>
       </div>
     );
