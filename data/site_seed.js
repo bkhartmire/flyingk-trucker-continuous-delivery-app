7bb7750d0a6fcd74ea12639de3335e8fe342cdda
Matthew Cook <mc255v@gmail.com>
2019-08-11 16:13:40 +0900

    added migration for data, seed file

--- data/site_seed.js
+++ data/site_seed.js
@@ -6,14 +6,13 @@ const db = require("../server/knex.js");
     const locations = JSON.parse(
       fs.readFileSync(__dirname + "/locations.json")
     );
-    console.log(locations[1]);
-    // for (const location of locations) {
-    //   const site = location;
-    //   const result = await db("data").insert({
-    //     site,
-    //   });
-    //   console.log(result);
-    // }
+    for (const location of locations) {
+      const site = location;
+      const result = await db("data").insert({
+        site,
+      });
+      console.log(result);
+    }
   } catch (err) {
     console.error("Error inserting records", err);
   }
