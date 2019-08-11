92c4177e7f095b9513354486eca1728efb262d24
bkhartmire <bkhartmire@gmail.com>
2019-08-12 08:37:48 +0900

    Revert "Revert "Filtering""

---
+++ migrations/10_add_data.js
@@ -0,0 +1,9 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("data", (table) => {
+    table.increments().index();
+
+    table.jsonb("site");
+  });
+};
+
+exports.down = function(knex, Promise) {};
