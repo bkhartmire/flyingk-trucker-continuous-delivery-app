6f3481554737fdd02412fe29bafb3743683c44ef
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-25 15:47:29 +0900

    remove uneeded runall commands

--- runall.sh
+++ runall.sh
@@ -6,5 +6,7 @@
 # ensure that migrations have been run ("yarn start" in bash)
 # run "./runall.sh" in bash at root level
 # program should exit on finish, do not terminate early
-node ./data/import.js && node ./data/import_restaurants.js && node ./data/import_amenities.js && node ./data/import_gas_types.js && node ./data/import_location_restaurants.js && node ./data/import_location_gas_types.js && node ./data/import_location_amenities.js && node ./data/site_seed.js;
+# node ./data/import.js && node ./data/import_restaurants.js && node ./data/import_amenities.js && node ./data/import_gas_types.js && node ./data/import_location_restaurants.js && node ./data/import_location_gas_types.js && node ./data/import_location_amenities.js
+# import json data directly to speed up load time 
+node ./data/site_seed.js;
 echo $?
