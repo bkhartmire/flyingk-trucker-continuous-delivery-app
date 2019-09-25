8d5e18fb5d199528747d4ab722ab55bebde3c3fd
Matthew Cook <mc255v@gmail.com>
2019-09-25 15:27:15 +0900

    inject all of locations into db

---
+++ data/site_seed.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const db = require("../server/knex.js");
+
+(async () => {
+  try {
+    const locations = JSON.parse(
+      fs.readFileSync(__dirname + "/locations.json")
+    );
+    for (const location of locations) {
+      const site = location;
+      const result = await db("data").insert({
+        site,
+      });
+      console.log(result);
+    }
+  } catch (err) {
+    console.error("Error inserting records", err);
+  }
+})();
