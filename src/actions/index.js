a5291d9824f387636a276023a25c4c91beec7f71
Brittany Hartmire <bkhartmire@gmail.com>
2019-09-30 11:29:50 +0900

    abstract select to work for unselecting as well

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,24 +21,15 @@ export function setStatesCities() {
   return { type: "SET_STATES_CITIES" };
 }
 
-export function filterType(e, type) {
+export function filterType(type) {
   return function(dispatch) {
-    e.target.checked
-      ? dispatch(selectType(type))
-      : dispatch(unselectType(type));
+    dispatch(selectType(type));
   };
 }
 
 function selectType(type) {
   return {
     type: "SELECT_TYPE",
-    payload: type,
-  };
-}
-
-function unselectType(type) {
-  return {
-    type: "UNSELECT_TYPE",
     payload: type,
   };
 }
